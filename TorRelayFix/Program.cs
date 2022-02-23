6e3314342cdbbfb42d6ea031155507f69da00b0f
ilusha <ilusha@dificen.to>
2022-02-23 19:00:33 +0300

    better timeout handling

--- TorRelayFix/Program.cs
+++ TorRelayFix/Program.cs
@@ -8,6 +8,8 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
 
 namespace TorRelayFix
 {
@@ -21,7 +23,7 @@ namespace TorRelayFix
             string json;
             string torPath = "";
             bool silent = false;
-
+            
             for (int i = 0; i < args.Length; i++)
             {
                 switch(args[i])
@@ -57,6 +59,7 @@ namespace TorRelayFix
                 Log("Tor Browser path was not specified, trying to find...", silent);
                 torPath = TryFindTor();
             }
+
             if (torPath == null)
             {
                 Log("Tor Browser folder not found, please specify (e.g program.exe --tor-folder \"F:\\Programs\\Tor Browser\\firefox.exe\")", silent);
@@ -77,14 +80,18 @@ namespace TorRelayFix
             Log("Got Relays: " + relays.Keys.Count, silent);
             Log("Fetching...", silent);
             Dictionary<string, string> workingRelays = new Dictionary<string, string>();
+            int counter = 0;
+            int BATCH_SIZE = 30;
             do
             {
-                relays = Pop(relays, out Dictionary<string, string> new_relays, 30);
+                counter += BATCH_SIZE;
+                relays = Pop(relays, out Dictionary<string, string> new_relays, BATCH_SIZE);
                 var temp = FetchRelays(new_relays).GetAwaiter().GetResult();
                 if (workingRelays.Count > 0)
                     foreach (var item in temp)
                         workingRelays.Add(item.Key, item.Value);
                 else workingRelays = temp;
+                Console.WriteLine($"WORKING RELAYS FOUND TOTAL: {workingRelays.Count}/5. Adresses scanned: {counter}");
 
             } while (workingRelays.Count < 5);
 
@@ -186,7 +193,7 @@ namespace TorRelayFix
 
         public static async Task<Dictionary<string, string>> FetchRelays(Dictionary<string, string> hostsFingerprints)
         {
-            var relays = hostsFingerprints.Select(x => Get($"https://{x.Key}/"));
+            var relays = hostsFingerprints.Select(x => GetAsync($"https://{x.Key}/"));
             var results = await Task.WhenAll(relays);
 
             Dictionary<string, string> workingRelays = new Dictionary<string, string>();
@@ -198,20 +205,50 @@ namespace TorRelayFix
             return workingRelays;            
         }
 
-        public static async Task<bool> Get(string uri)
+        static async Task<bool> GetAsync(string uri)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-            try
+            int[] ports = { 1, 7, 9, 11, 13, 15, 17, 19, 20, 21, 22, 23, 25, 37, 42, 43, 53, 69, 77, 79, 87,
+                95, 101, 102, 103, 104, 109, 110, 111, 113, 115, 117, 119, 123, 135, 137, 139, 143, 161, 179,
+                389, 427, 465, 512, 513, 514, 515, 526, 530, 531, 532, 540, 548, 554, 556, 563, 587, 601, 636,
+                989, 990, 993, 995, 1719, 1720, 1723, 2049, 3659, 4045, 5060, 5061, 6000, 6566, 6665, 6666,
+                6667, 6668, 6669, 6697, 10080 };
+            var port = int.Parse(uri.Split(':')[2].Split('/')[0]);
+            if (ports.Contains(port)) return false;
+            var handler = new TimeoutHandler
             {
-                HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync();
-            }
-            catch (WebException ex)
+                DefaultTimeout = TimeSpan.FromSeconds(5),
+                InnerHandler = new HttpClientHandler()
+            };
+
+            using (var cts = new CancellationTokenSource())
+            using (var client = new HttpClient(handler))
             {
-                if (ex.Status == WebExceptionStatus.Timeout || ex.Status == WebExceptionStatus.ConnectFailure)
+                client.Timeout = Timeout.InfiniteTimeSpan;
+
+                var request = new HttpRequestMessage(HttpMethod.Get, uri);
+                try
+                {
+                    using (var response = await client.SendAsync(request, cts.Token))
+                    {
+                        Console.WriteLine($"{uri} response: {response.StatusCode}");
+                    }
+                }
+                
+                catch (HttpRequestException ex) when (ex.InnerException is WebException)
+                {
+                    if ((ex.InnerException as WebException).Status == WebExceptionStatus.ConnectFailure)
+                        return false;
+                }
+                catch (HttpRequestException ex)
+                {
+                    //Console.WriteLine(ex);
+                }
+                catch (TimeoutException)
+                {
                     return false;
+                }
+                return true;
             }
-            return true;
         }
 
         public static Dictionary<string, string> Parse(string json)
@@ -226,6 +263,60 @@ namespace TorRelayFix
                     relays.Add(addr.Groups["Value"].Value, fp.Groups["Value"].Value); 
             }
             return relays;
+        }
+    }
+
+    public static class HttpRequestExtensions
+    {
+        private const string TimeoutPropertyKey = "RequestTimeout";
+
+        public static void SetTimeout(this HttpRequestMessage request, TimeSpan? timeout)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            request.Properties[TimeoutPropertyKey] = timeout;
+        }
+
+        public static TimeSpan? GetTimeout(this HttpRequestMessage request)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            if (request.Properties.TryGetValue(TimeoutPropertyKey, out var value) && value is TimeSpan timeout)
+                return timeout;
+            return null;
+        }
+    }
+
+    public class TimeoutHandler : DelegatingHandler
+    {
+        public TimeSpan DefaultTimeout { get; set; } = TimeSpan.FromSeconds(100);
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            using (var cts = GetCancellationTokenSource(request, cancellationToken))
+            {
+                try
+                {
+                    return await base.SendAsync(request, cts?.Token ?? cancellationToken);
+                }
+                catch (OperationCanceledException) when (!cancellationToken.IsCancellationRequested)
+                {
+                    throw new TimeoutException();
+                }
+            }
+        }
+
+        private CancellationTokenSource GetCancellationTokenSource(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var timeout = request.GetTimeout() ?? DefaultTimeout;
+            if (timeout == Timeout.InfiniteTimeSpan)
+            {
+                return null;
+            }
+            else
+            {
+                var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                cts.CancelAfter(timeout);
+                return cts;
+            }
         }
     }
 }
